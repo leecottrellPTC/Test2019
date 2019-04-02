93eaf8340365571ba4258cbe8fa886742bce61c6
Lee Cottrell <cottrell.lee@ptcollege.edu>
2019-04-02 15:42:21 -0400

    Update Test2019.cpp

--- Test2019/Test2019.cpp
+++ Test2019/Test2019.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "This works\n"; 
+	cout << "How do I commit?\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
